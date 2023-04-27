f175bb7cb00d2b671c88259f62649b9242022903
uhlers <uhlers@my.erau.edu>
2023-04-27 13:19:46 -0700

    Corrected output for incorrect inputs.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,31 @@
-"""
-odd/even number checker
-Author: Wolf Paulus (https://wolfpaulus.com)
+""""
+Is divisible by 3 checker
+Author: Skyler Uhler
 """
 
 
 def is_div3(num: int) -> bool:
     """Return True if num is divisible by 3, False otherwise."""
-    snumber = str(num)
     sumnum = 0
-    for i in snumber:
+    sumnum2 = 0
+    for i in str(num):
         sumnum += int(i)
 
-    return sumnum % 3 == 0
+    if len(str(sumnum)) > 1:
+        for i in str(sumnum):
+            sumnum2 += int(i)
+
+    if sumnum == 3 or sumnum == 6 or sumnum == 9:
+        return True
+    elif sumnum2 == 3 or sumnum2 == 6 or sumnum2 == 9:
+        return True
+    else:
+        return False
 
 
 def is_div3_str(num: str) -> str:
-    """Return a string indicating whether num is odd or even."""
+    """Return a string indicating whether num is divisible by 3."""
     if num.isnumeric():
         return f"{num} is {'divisible by 3' if is_div3(int(num)) else 'not divisible by 3'}."
     else:
-        return "Please enter a number."
+        return f"Please enter a number"
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,8 +13,10 @@ class Test(TestCase):
         assert not is_div3(635)
         assert is_div3(963)
 
+
     def test_is_div3_str(self):
         assert is_div3_str("5") == "5 is not divisible by 3."
         assert is_div3_str("6") == "6 is divisible by 3."
         assert is_div3_str("635") == "635 is not divisible by 3."
         assert is_div3_str("963") == "963 is divisible by 3."
+        assert is_div3_str("A") == "Please enter a number"
